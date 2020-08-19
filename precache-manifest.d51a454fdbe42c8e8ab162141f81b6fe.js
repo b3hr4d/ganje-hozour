752901b20c54dcfeda33ade999756f734e37ebca
b3hr4d <behradmusiclove@yahoo.com>
2020-08-19 15:01:09 +0430

    Updates

---
+++ precache-manifest.d51a454fdbe42c8e8ab162141f81b6fe.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a68b65babf7f117742e53c46e9194c2",
+    "url": "/Ganje-Hozour/index.html"
+  },
+  {
+    "revision": "8af72cc6729f4bdd5769",
+    "url": "/Ganje-Hozour/static/css/main.ef8d1a29.chunk.css"
+  },
+  {
+    "revision": "9c3dfc75c139678399f1",
+    "url": "/Ganje-Hozour/static/js/2.8b09594f.chunk.js"
+  },
+  {
+    "revision": "8dbc9bd92b344a5deca62eeacc09fc2d",
+    "url": "/Ganje-Hozour/static/js/2.8b09594f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af72cc6729f4bdd5769",
+    "url": "/Ganje-Hozour/static/js/main.bb58e777.chunk.js"
+  },
+  {
+    "revision": "84edf1459c1990215213",
+    "url": "/Ganje-Hozour/static/js/runtime-main.f8204640.js"
+  },
+  {
+    "revision": "083395bb280392dbac754b0aba022ad1",
+    "url": "/Ganje-Hozour/static/media/IranNastaliq.083395bb.ttf"
+  },
+  {
+    "revision": "b495bc14c98fd308bafefcfd31fddd20",
+    "url": "/Ganje-Hozour/static/media/Vazir-Black.b495bc14.woff2"
+  },
+  {
+    "revision": "ed4a58c261df0539ab08fc9064b20e33",
+    "url": "/Ganje-Hozour/static/media/Vazir-Light.ed4a58c2.woff2"
+  },
+  {
+    "revision": "4bd4185d80a8f3bc85f1995457ea1e42",
+    "url": "/Ganje-Hozour/static/media/Vazir-Medium.4bd4185d.woff2"
+  },
+  {
+    "revision": "dfc05881920f7bc8593ca145a79407dd",
+    "url": "/Ganje-Hozour/static/media/Vazir-Thin.dfc05881.woff2"
+  },
+  {
+    "revision": "95ee69c4226f615b56f51548f5c932b1",
+    "url": "/Ganje-Hozour/static/media/Vazir.95ee69c4.woff2"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/Ganje-Hozour/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/Ganje-Hozour/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/Ganje-Hozour/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/Ganje-Hozour/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/Ganje-Hozour/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/Ganje-Hozour/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/Ganje-Hozour/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/Ganje-Hozour/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/Ganje-Hozour/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/Ganje-Hozour/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/Ganje-Hozour/static/media/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "4cba7800dfc823962dbc715d282bdc1b",
+    "url": "/Ganje-Hozour/static/media/god.4cba7800.jpeg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/Ganje-Hozour/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/Ganje-Hozour/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/Ganje-Hozour/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/Ganje-Hozour/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/Ganje-Hozour/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/Ganje-Hozour/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/Ganje-Hozour/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/Ganje-Hozour/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/Ganje-Hozour/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/Ganje-Hozour/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/Ganje-Hozour/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/Ganje-Hozour/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/Ganje-Hozour/static/media/studio-5.ef5c30ea.jpg"
+  }
+]);
